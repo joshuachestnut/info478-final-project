429b1d377ff3669df19df3ce963ffbe9d3e6aff2
Joshua Chestnut <jchest19@uw.edu>
2019-05-19 21:28:32 -0700

    structure the ui, server, and app by sections of the project

---
+++ shiny_app/server.R
@@ -0,0 +1,7 @@
+source("ui.R")
+
+# 1. Allocation of money spent on food, how that relates to food insecurity?
+
+# 2. Map of Adult vs. Child Food Insecurity
+
+# 3. Trends of Food Insecurity based on different demogrpahics
---
+++ shiny_app/ui.R
@@ -0,0 +1,10 @@
+library("dplyr")
+library("ggplot2")
+library("shiny")
+library("rsconnect")
+
+# 1. Allocation of money spent on food, how that relates to food insecurity?
+
+# 2. Map of Adult vs. Child Food Insecurity
+
+# 3. Trends of Food Insecurity based on different demogrpahics
